a0ae30b291ab7d53ee92dd10fcd9fcd66039ebb1
manas9-sys <manas885394937@gmail.com>
2026-08-22 08:57:09 +0530

    a

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': {
+      'https://github-analyzer1-b1cm.onrender.com': {
         target: 'https://github-analyzer1-b1cm.onrender.com',
         changeOrigin: true
       }
